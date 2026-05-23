955f119a7e10924fe7bfb6ad307352923300a345
Gabriela Sales <63962255+gabrielacsales@users.noreply.github.com>
2026-05-23 23:56:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Learning how to clone https://www.tabnews.com.br an Aplication from Filipe Deschamps on his course 'https://curso.dev'
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Ei, eu te amo! Se você me ama, dá uma risadinha! 😁</h1>
+  return <h1>Ei, eu te amo! Se você me ama, dá uma risadinha! 😁</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
